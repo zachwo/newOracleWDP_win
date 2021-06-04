d44c5931b831d6a5ae0f50e7d978a3aa982265a5
wangze <zachayrwong880@outlook.com>
2021-06-04 10:42:58 +0800

    练习

---
+++ 2021_04_06/src/com/implement/IpadPro.java
@@ -0,0 +1,13 @@
+package com.implement;
+
+public class IpadPro implements Ipad{
+    @Override
+    public void play() {
+        System.out.println("play ipad game");
+    }
+
+    @Override
+    public void music() {
+        System.out.println("listen  apple music");
+    }
+}
