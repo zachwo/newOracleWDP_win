37965e148e3c2e42d2897216cf6e2b8a96dee136
wangze <zacharywong880@outlook.com>
2021-06-21 22:04:35 +0800

    练习

--- 2021_04_21/src/com/udp/Server.java
+++ 2021_04_21/src/com/udp/Server.java
@@ -8,7 +8,7 @@ public class Server {
     public static void main(String[] args) throws IOException {
         DatagramSocket ds = new DatagramSocket(8080);
         byte[] b = new byte[1024];
-        //构造数据报包，用来接收长度为 length 的包，在缓冲区中指定了偏移量 。
+        //构造数据报包，用来接收长度为 length 的包，在缓冲区中指定了偏移量  。
         DatagramPacket dp = new DatagramPacket(b,0,b.length);
         System.out.println("waiting for receive");
         ds.receive(dp);
