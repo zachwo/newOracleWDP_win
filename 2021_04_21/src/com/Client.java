0763eb83f7150de17b68b5f7e9bd28039f8519bf
wangze <zacharywong880@outlook.com>
2021-06-21 14:25:01 +0800

    练习

---
+++ 2021_04_21/src/com/Client.java
@@ -0,0 +1,27 @@
+package com;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Socket s = new Socket("127.0.0.1",8888);
+        OutputStream os = s.getOutputStream();
+        os.write("服务器你好，我是客户机".getBytes());
+        os.flush();
+
+        //接收服务器信息
+        InputStream is = s.getInputStream();
+        byte[] b = new byte[1024];
+        String str = "";
+        int length = is.read(b);
+        while (length != -1){
+            str += new String(b,0,length);
+            length = is.read(b);
+        }
+        System.out.println(str);
+        os.close();
+    }
+}
---
+++ 2021_04_21/src/com/Server.java
@@ -0,0 +1,39 @@
+package com;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+//socket对象类似于两个人打电话时候用的手机，两个人交流的时候传输的是语音信号，而不是手机
+//accept方法会一直监听客户端发过来的信息，如果有客户端发过来信息，则accept方法会自己生成一个socket对象并接收信息
+//注意客户端的socket对象和accept方法生成的socket对象不是同一个，socket对象只是传递信息的一个媒介
+//socket对象联系两个程序，只要其中一方关闭了socket连接，整个通信就断了
+
+public class Server {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ServerSocket ss = new ServerSocket(8888);
+        System.out.println("等待连接...");
+        Socket s = ss.accept();//生成Socket对象以接收客户机的信息
+        System.out.println(s.getInetAddress().getHostAddress()+"连接成功");
+
+        InputStream is = s.getInputStream();//输入流接收客户端发出的字节数组
+        byte[] b = new byte[1024];
+        String str = "";
+        int length = is.read(b);
+        //此处有bug，原因未知
+//        while (length != -1){
+//            str += new String(b,0,length);
+//            length = is.read(b);
+//        }
+        str = new String(b, 0, length);
+        System.out.println(str);
+
+        //服务器返回消息
+        OutputStream os = s.getOutputStream();
+        os.write("这是一条来自服务器的回应".getBytes());
+        os.flush();
+        os.close();
+
+    }
+}
