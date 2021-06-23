179280c70a49b0b04f4879171691dbb9f0f7701f
wangze <zacharywong880@outlook.com>
2021-06-23 14:08:11 +0800

    练习

--- 2021_04_23/src/com/CallableTest/Test.java
+++ 2021_04_23/src/com/CallableTest/Test.java
@@ -1,9 +1,8 @@
 package com.CallableTest;
 
-import java.util.concurrent.ExecutorService;
-
+//实现多线程的第三种方式：实现Callable接口 重写 call方法
 public class Test {
     public static void main(String[] args) {
-//        ExecutorService es =
+
     }
 }
